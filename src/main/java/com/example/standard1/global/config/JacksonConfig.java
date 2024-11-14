1b768eafda0043d10a1ff9c70d58f0f2230c2135
kimdongjoon-web <color6763@naver.com>
2024-11-14 16:26:53 +0900

    feat: JacksonConfig 추가

---
+++ src/main/java/com/example/standard1/global/config/JacksonConfig.java
@@ -0,0 +1,11 @@
+package com.example.standard1.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+@Configuration
+@EnableSpringDataWebSupport(
+        pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO
+)
+public class JacksonConfig {
+}
