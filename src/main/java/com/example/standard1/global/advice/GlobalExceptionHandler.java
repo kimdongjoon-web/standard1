3f1dc7ba1a6434f4036a73474d59a186808216d9
kimdongjoon-web <color6763@naver.com>
2024-11-14 16:26:42 +0900

    feat: GlobalExceptionHandler 추가

---
+++ src/main/java/com/example/standard1/global/advice/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.example.standard1.global.advice;
+
+import com.example.standard1.global.exception.BaseException;
+import com.example.standard1.global.response.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BaseException.class)
+    public ResponseEntity<ApiResponse<Void>> handleCustomException(BaseException e) {
+        return ResponseEntity
+                .status(e.getErrorCode().getHttpStatus())
+                .body(ApiResponse.error(e.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Void>> handleException(Exception e) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ApiResponse.error("Internal server error"));
+    }
+}
