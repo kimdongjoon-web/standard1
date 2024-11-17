0efde093b584c841bbe8f60fed8cdee9c6c697b9
kimdongjoon-web <color6763@naver.com>
2024-11-17 21:16:00 +0900

    feat: ArticleResponse 추가

---
+++ src/main/java/com/example/standard1/domain/article/dto/response/ArticleResponse.java
@@ -0,0 +1,16 @@
+package com.example.standard1.domain.article.dto.response;
+
+import com.example.standard1.domain.article.domain.Article;
+import lombok.Getter;
+
+@Getter
+public class ArticleResponse {
+
+    private final String title;
+    private final String content;
+
+    public ArticleResponse(Article article) {
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+}
