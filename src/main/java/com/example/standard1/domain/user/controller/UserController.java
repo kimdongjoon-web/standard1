058dcfc8496e65f5e9f4f04df46354f3fab3253a
동준 <color6763@naver.com>
2024-12-25 00:30:30 +0900

    mod : 컨트롤러 코드 간소화

--- src/main/java/com/example/standard1/domain/user/controller/UserController.java
+++ src/main/java/com/example/standard1/domain/user/controller/UserController.java
@@ -4,7 +4,6 @@ import com.example.standard1.domain.user.dto.request.UserCreateRequest;
 import com.example.standard1.domain.user.dto.request.UserUpdateRequest;
 import com.example.standard1.domain.user.dto.response.UserResponse;
 import com.example.standard1.domain.user.service.UserService;
-import com.example.standard1.global.response.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -25,37 +24,36 @@ public class UserController {
 
     // Create
     @PostMapping("/signup")
-    public ResponseEntity<ApiResponse<UserResponse>> create(@Valid @RequestBody UserCreateRequest request) {
+    public ResponseEntity<UserResponse> create(@Valid @RequestBody UserCreateRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(ApiResponse.success(userService.create(request)));
+                .body(userService.create(request));
     }
 
     // Read
     @GetMapping("/{id}")
-    public ApiResponse<UserResponse> getById(@PathVariable Long id) {
-        UserResponse user = userService.getById(id);
-        return ApiResponse.success(user);
+    public ResponseEntity<UserResponse> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getById(id));
     }
 
     // Read(List)
     @GetMapping
-    public ResponseEntity<ApiResponse<Page<UserResponse>>> getList(
+    public ResponseEntity<Page<UserResponse>> getList(
             @PageableDefault(size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
-        return ResponseEntity.ok(ApiResponse.success(userService.getList(pageable)));
+        return ResponseEntity.ok(userService.getList(pageable));
     }
 
     // Update
     @PutMapping("/{id}")
-    public ResponseEntity<ApiResponse<UserResponse>> update(
+    public ResponseEntity<UserResponse> update(
             @PathVariable Long id,
             @Valid @RequestBody UserUpdateRequest request) {
-        return ResponseEntity.ok(ApiResponse.success(userService.update(id, request)));
+        return ResponseEntity.ok(userService.update(id, request));
     }
 
     // Delete
     @DeleteMapping("/{id}")
-    public ResponseEntity<ApiResponse<Void>> delete(@PathVariable Long id) {
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         userService.delete(id);
-        return ResponseEntity.ok(ApiResponse.success(null));
+        return ResponseEntity.ok(null);
     }
 }
